bf4afeb1f9964f8f45293b8e9ee1c21958ac569f
dan lima <danieldelimamendes500@gmail.com>
2024-04-07 21:39:28 -0300

    .

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ $(document).ready(myApp)
 
 function myApp(){
 
-
+// verifica se o session storage contem alguma rota
+if (sessionStorage.path == undefined) {
+  //se não houver, aponta para index raiz
+  sessionStorage.path = 'index.html'}
+  path = sessionStorage.path
+  delete sessionStorage.path
+  // carrega a página solicitada pela rota
+  loadpage(path)
+  
+  $(document).on('click', 'a', routerlink)
 }
 
 
-// verifica se o session storage contem alguma rota
-if (sessionStorage.path == undefined) {
-//se não houver, aponta para index raiz
-sessionStorage.path = 'index.html'}
-path = sessionStorage.path
-delete sessionStorage.path
-// carrega a página solicitada pela rota
-loadpage(path)
 
-$(document).on('click', 'a', routerlink)
 
