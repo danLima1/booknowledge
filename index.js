ae3224670a4bea848e831215348449814a1f6aec
dan lima <danieldelimamendes500@gmail.com>
2024-04-07 21:25:34 -0300

    .

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 $(document).ready(myApp)
 
 function myApp(){
-  
+
+
 }
+
 
 // verifica se o session storage contem alguma rota
 if (sessionStorage.path == undefined) 
 //se não houver, aponta para index raiz
-sessionStorage.path = ''
+sessionStorage.path = 'index.html'
 path = sessionStorage.path
 delete sessionStorage.path
 // carrega a página solicitada pela rota
 loadpage(path)
 
 $(documente).on('click', 'a', routerlink)
-
-
-
-
