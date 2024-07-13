28a353f6f5122f19127eaa51ea473400f1903914
dan lima <danieldelimamendes500@gmail.com>
2024-07-12 21:10:52 -0300

    .

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,24 +1,18 @@
 $(document).ready(function() {
     $('#search-input').on('input', function() {
         const searchValue = $(this).val().toLowerCase();
-        if (searchValue === '') {
-            $('.card-recentes').show(); // Show all cards if search input is empty
-        } else {
-            $('.card-recentes').each(function() {
-                const titles = $(this).find('.title-text');
-                let match = false;
-                titles.each(function() {
-                    if ($(this).text().toLowerCase().includes(searchValue)) {
-                        match = true;
-                        return false; // Break out of the loop
-                    }
-                });
-                if (match) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
+        $('.card-recentes').each(function() {
+            let match = false;
+            $(this).find('.title-text').each(function() {
+                if ($(this).text().toLowerCase().includes(searchValue)) {
+                    match = true;
                 }
             });
-        }
+            if (match) {
+                $(this).closest('tr').show(); // Mostrar a linha da tabela se algum título corresponder à pesquisa
+            } else {
+                $(this).closest('tr').hide(); // Ocultar a linha da tabela se nenhum título corresponder à pesquisa
+            }
+        });
     });
 });
