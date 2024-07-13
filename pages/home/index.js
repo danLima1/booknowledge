5846a60e0be955b0c29a39b429db72757ddaa2b1
dan lima <danieldelimamendes500@gmail.com>
2024-07-12 21:20:30 -0300

    .

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
     $('#search-input').on('input', function() {
         const searchValue = $(this).val().toLowerCase();
-        $('.card-recentes').each(function() {
-            let match = false;
-            $(this).find('.title-text').each(function() {
-                if ($(this).text().toLowerCase().includes(searchValue)) {
-                    match = true;
+        let matchFound = false;
+        
+        if (searchValue === '') {
+            $('.card-recentes').show(); // Mostrar todos os cartões se o campo de busca estiver vazio
+            $('.no-results').hide(); // Esconder a mensagem de "nenhum resultado"
+        } else {
+            $('.card-recentes').each(function() {
+                let match = false;
+                $(this).find('.title-text').each(function() {
+                    if ($(this).text().toLowerCase().includes(searchValue)) {
+                        match = true;
+                    }
+                });
+                if (match) {
+                    $(this).show(); // Mostrar a linha da tabela se algum título corresponder à pesquisa
+                    matchFound = true;
+                } else {
+                    $(this).hide(); // Ocultar a linha da tabela se nenhum título corresponder à pesquisa
                 }
             });
-            if (match) {
-                $(this).closest('tr').show(); // Mostrar a linha da tabela se algum título corresponder à pesquisa
+
+            if (matchFound) {
+                $('.no-results').hide(); // Esconder a mensagem de "nenhum resultado"
             } else {
-                $(this).closest('tr').hide(); // Ocultar a linha da tabela se nenhum título corresponder à pesquisa
+                $('.no-results').show(); // Mostrar a mensagem de "nenhum resultado"
             }
-        });
+        }
     });
 });
